8e27aa4bf9149712207f3c44b5391c0f1d6dd087
Sam <samueljshaaban@gmail.com>
2025-02-26 19:40:42 -0800

    Revert "Initial changes for controls tests."
    This reverts commit 384e0e3555fe09896acc62a2fd47bf6c83616f8e.

--- Oscilloscope.ino
+++ Oscilloscope.ino
@@ -1,8 +1,6 @@
 #include "DataTypes.h"
 #include "InputAcquisition.h"
-
-void initGraphics() {}
-void updateGraphics(Buffer &CH1, Buffer &CH2, DisplayAdjust &scale, Trigger &triggerSettings) {}
+#include "Graphics.h"
 
 Buffer CH1, CH2;
 DisplayAdjust display;
@@ -15,37 +13,12 @@ const unsigned long frameInterval = 50;  // Time interval between frames (50 ms 
 void setup() {
   initInput();
   initGraphics();
-  Serial.begin(115200);
-  Serial.println("Setup complete");
 }
 
 void loop() {
-  if(acquireInput(CH1, CH2, display, trigger)) {
-    Serial.println("Channel 1");
-    Serial.println(display.CH1Scale);
-    Serial.println(display.CH1Shift);
-
-    Serial.println("Channel 2");
-    Serial.println(display.CH2Scale);
-    Serial.println(display.CH2Shift);
-
-    Serial.println("Time, Trigger");
-    Serial.println(display.timeScale);
-    Serial.println(trigger.val);
-
-    Serial.println("Enables: 1, 2");
-    Serial.println(CH1.enabled());
-    Serial.println(CH2.enabled());
-
-    Serial.println("Trigger: Enable, decrease, CH2");
-    Serial.println(trigger.enable);
-    Serial.println(trigger.decrease);
-    Serial.println(trigger.CH2);
-  }
-  /*
+  if(acquireInput(CH1, CH2, display, trigger))
     if (millis() - lastUpdate >= frameInterval) {
       updateGraphics(CH1, CH2, display, trigger);
       lastUpdate = millis();
     }
-    */
 }
